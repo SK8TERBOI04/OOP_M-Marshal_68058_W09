9ed2d30713efa204e858ed89a5937129e0e40229
SK8TERBOI04 <150426448+SK8TERBOI04@users.noreply.github.com>
2025-11-12 11:33:54 +0700

    Interface Animal

---
+++ src/Latihan/Animal.java
@@ -0,0 +1,6 @@
+package Latihan;
+
+public interface Animal {
+    public void eat();
+    public void travel();
+}
---
+++ src/Latihan/Mammal.java
@@ -0,0 +1,20 @@
+package Latihan;
+
+public class Mammal implements Animal {
+
+    public Mammal() {}
+
+    public void eat() {
+        System.out.println("Mammal eats");
+    }
+
+    public void travel() {
+        System.out.println("Mammal travel");
+    }
+
+    public static void main(String[] args) {
+        Mammal m = new Mammal();
+        m.eat();
+        m.travel();
+    }
+}
